46940bb5adadbe7ad57efbc8def7f337f265df9f
Ayrton Paredes <ayrton@thinkanddev.com>
2023-11-19 06:14:38 +0300

    styles

--- src/app/order/[orderId]/page.tsx
+++ src/app/order/[orderId]/page.tsx
@@ -26,51 +26,40 @@ const OrderDetail = () => {
                 <div className="pb-4 md:pb-8 w-full md:w-40">
                   <img
                     className="w-full hidden md:block"
-                    src="https://i.ibb.co/84qQR4p/Rectangle-10.png"
-                    alt="dress"
-                  />
-                  <img
-                    className="w-full md:hidden"
-                    src="https://i.ibb.co/L039qbN/Rectangle-10.png"
-                    alt="dress"
+                    src="/noun-1.png"
+                    alt="noun"
+                    width={140}
                   />
                 </div>
                 <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
                   <div className="w-full flex flex-col justify-start items-start space-y-8">
                     <h3 className="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800">
-                      {address}
+                      Emmilia Mertens
                     </h3>
                     <div className="flex justify-start items-start flex-col space-y-2">
                       <p className="text-sm dark:text-white leading-none text-gray-800">
                         <span className="dark:text-gray-400 text-gray-300">
-                          Style:{" "}
+                          Date:{" "}
                         </span>{" "}
-                        Italic Minimal Design
+                        {new Date().toISOString().slice(0, 10)}
                       </p>
                       <p className="text-sm dark:text-white leading-none text-gray-800">
                         <span className="dark:text-gray-400 text-gray-300">
-                          Size:{" "}
+                          Hour:{" "}
                         </span>{" "}
-                        Small
-                      </p>
-                      <p className="text-sm dark:text-white leading-none text-gray-800">
-                        <span className="dark:text-gray-400 text-gray-300">
-                          Color:{" "}
-                        </span>{" "}
-                        Light Blue
+                        {new Date().toISOString().slice(11, 16)}
                       </p>
                     </div>
                   </div>
                   <div className="flex justify-between space-x-8 items-start w-full">
                     <p className="text-base dark:text-white xl:text-lg leading-6">
-                      $36.00{" "}
-                      <span className="text-red-300 line-through"> $45.00</span>
+                      $195.00
                     </p>
                     <p className="text-base dark:text-white xl:text-lg leading-6 text-gray-800">
-                      01
+                      0.1
                     </p>
                     <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">
-                      $36.00
+                      ETH
                     </p>
                   </div>
                 </div>
@@ -79,51 +68,40 @@ const OrderDetail = () => {
                 <div className="w-full md:w-40">
                   <img
                     className="w-full hidden md:block"
-                    src="https://i.ibb.co/s6snNx0/Rectangle-17.png"
-                    alt="dress"
-                  />
-                  <img
-                    className="w-full md:hidden"
-                    src="https://i.ibb.co/BwYWJbJ/Rectangle-10.png"
-                    alt="dress"
+                    src="/noun-3.png"
+                    alt="noun"
+                    width={140}
                   />
                 </div>
                 <div className="flex justify-between items-start w-full flex-col md:flex-row space-y-4 md:space-y-0">
                   <div className="w-full flex flex-col justify-start items-start space-y-8">
                     <h3 className="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800">
-                      High Quaility Italic Dress
+                      Freddy Chambi
                     </h3>
                     <div className="flex justify-start items-start flex-col space-y-2">
                       <p className="text-sm dark:text-white leading-none text-gray-800">
                         <span className="dark:text-gray-400 text-gray-300">
-                          Style:{" "}
+                          Date:{" "}
                         </span>{" "}
-                        Italic Minimal Design
+                        {new Date().toISOString().slice(0, 10)}
                       </p>
                       <p className="text-sm dark:text-white leading-none text-gray-800">
                         <span className="dark:text-gray-400 text-gray-300">
-                          Size:{" "}
+                          Hour:{" "}
                         </span>{" "}
-                        Small
-                      </p>
-                      <p className="text-sm dark:text-white leading-none text-gray-800">
-                        <span className="dark:text-gray-400 text-gray-300">
-                          Color:{" "}
-                        </span>{" "}
-                        Light Blue
+                        {new Date().toISOString().slice(11, 16)}
                       </p>
                     </div>
                   </div>
                   <div className="flex justify-between space-x-8 items-start w-full">
                     <p className="text-base dark:text-white xl:text-lg leading-6">
-                      $20.00{" "}
-                      <span className="text-red-300 line-through"> $30.00</span>
+                      $20.00
                     </p>
                     <p className="text-base dark:text-white xl:text-lg leading-6 text-gray-800">
-                      01
+                      20.0
                     </p>
                     <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">
-                      $20.00
+                      DAI
                     </p>
                   </div>
                 </div>
